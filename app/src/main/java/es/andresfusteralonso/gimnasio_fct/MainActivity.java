2f12abea27236776b8a5d043fc237e1d4dad986f
Andres Fuster Alonso <andresfusteralonso@gmail.com>
2023-05-01 22:54:09 +0200

    conexion base de datos

--- app/src/main/java/es/andresfusteralonso/gimnasio_fct/MainActivity.java
+++ app/src/main/java/es/andresfusteralonso/gimnasio_fct/MainActivity.java
@@ -1,21 +1,75 @@
 package es.andresfusteralonso.gimnasio_fct;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class ConnectionClass {
+        String classs = "com.mysql.cj.jdbc.Driver";
+        //String ip = "localhost";
+        String ip = "10.0.2.2";
+        String baseDatos = "gimnasio";
+        String url = "jdbc:mysql://";
+        String un = "root";
+        String password = "root";
+
+
+        @SuppressLint("NewApi")
+        public Connection CONN() {
+            /*
+            Connection conn = null;
+            try {
+                 //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/gimnasio", "root", "root");
+                conn = DriverManager.getConnection(url + ip + ":3306/" + baseDatos, "root", "root");
+            } catch (Exception e) {
+                Log.e("Exception error", e.getMessage());
+            }
+
+            return conn;*/
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            Connection conn = null;
+            String ConnURL = null;
+            try {
+
+                Class.forName(classs);
+
+                conn = DriverManager.getConnection(url + ip + ":3306/" + baseDatos, un, password);
+
+            } catch (SQLException se) {
+                Log.e("ERRO", se.getMessage());
+            } catch (ClassNotFoundException e) {
+                Log.e("ERRO", e.getMessage());
+            } catch (Exception e) {
+                Log.e("ERRO", e.getMessage());
+            }
+            return conn;
+
+
+
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,55 +84,73 @@ public class MainActivity extends AppCompatActivity {
         botonNavegar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                /*
                 try {
-                    // Conectar a la base de datos
-                    Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/gimnasio", "root", "root");
-
-                // Crear la consulta SQL con el valor del campo de texto
-                    String consulta = "SELECT * FROM clientes WHERE contrasena = ?";
-                    PreparedStatement sentencia = conexion.prepareStatement(consulta);
-                    sentencia.setString(1, contrasena);
-
-                // Ejecutar la consulta
-                    ResultSet resultado = sentencia.executeQuery();
-
-                    // Crear la consulta SQL con el valor del campo de texto
-                    String consulta1 = "SELECT * FROM monitores WHERE contrasena = ?";
-                    PreparedStatement sentencia1 = conexion.prepareStatement(consulta);
-                    sentencia.setString(1, contrasena);
-
-                    // Ejecutar la consulta
-                    ResultSet resultado1 = sentencia.executeQuery();
-
-                    // Procesar los resultados de la consulta
-                    if (resultado.next()) {
-                        // El dato pertenece a la tabla clientes
-                        Intent intent = new Intent(getApplicationContext(), Horario_Clientes.class);
-                        startActivity(intent);
-                    } else if (resultado1.next()) {
-                        // El dato pertenece a la monitores
-                        Intent intent = new Intent(getApplicationContext(), MenuPrincipal.class);
-                        startActivity(intent);
+                    Connection con = new ConnectionClass().CONN();
+                    if (con != null) {
+                        PreparedStatement statement = con.prepareStatement("select * from clientes");
+                        ResultSet result = statement.executeQuery();
+                        Log.println(Log.INFO, "INFO", "Correcto");
                     } else {
-                        // El dato no pertenece a ninguna de las tablas
-                        System.out.println("El dato no pertenece a ninguna tabla");
+                        Log.e("ERROR:" ,"La conexión es null");
                     }
-
-                    // Cerrar la conexión, la declaración y el resultado
-                    resultado.close();
-                    resultado1.close();
-                    sentencia.close();
-                    sentencia1.close();
-                    conexion.close();
                 } catch (Exception e) {
+                    Log.e("ERROR: ", e.getMessage());
+                }*/
+
+
+                APIJson api = new APIJson();
+                try {
+                    JSONObject object = api.readJsonFromUrl("http://10.0.2.2:1337/monitores");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
 
 
 
+                /*
+                Connection con;
+                try {
+                    // Conectar a la base de datos
+                    con = (Connection) new ConnectionClass().CONN();
 
+                    // Crear la consulta SQL con el valor del campo de texto
+                    PreparedStatement consultaClientes = con.prepareStatement("SELECT gimnasio.* FROM clientes;");
+                    consultaClientes.setString(1, contrasena);
+                    ResultSet resultadoClientes = consultaClientes.executeQuery();
+
+                    // Crear la consulta SQL con el valor del campo de texto
+                    PreparedStatement consultaMonitores = con.prepareStatement("SELECT gimnasio.* FROM monitores;");
+                    consultaMonitores.setString(1, contrasena);
+                    ResultSet resultadoMonitores = consultaClientes.executeQuery();
+
+
+
+                    // Procesar los resultados de la consulta
+                    if (resultadoClientes.next()) {
+                        // El dato pertenece a la tabla clientes
+                        Intent intent = new Intent(getApplicationContext(), Horario_Clientes.class);
+                        startActivity(intent);
+                    } else if (resultadoMonitores.next()) {
+                        // El dato pertenece a la tabla monitores
+                        Intent intent = new Intent(getApplicationContext(), MenuPrincipal.class);
+                        startActivity(intent);
+                    }
+
+                    // Cerrar la conexión, la declaración y el resultado
+                    resultadoClientes.close();
+                    consultaClientes.close();
+                    resultadoMonitores.close();
+                    consultaMonitores.close();
+                    con.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }*/
             }
         });
     }
+
+
 }
